4ce379c0c96025f19227d901cfa4f4a7a4059c11
jp-sena-dev <joaopedrosena1997@gmail.com>
2025-04-02 22:53:55 -0300

    create points

--- points.js
+++ points.js
@@ -1,6 +1,9 @@
 const points = [
   {
-    location: [-15.822355945358478, -47.979178936103075],
+    location: {
+      lat: -15.822355945358478,
+      lng: -47.979178936103075
+    },
     name: 'Atacadão dia a dia - Taguatinga',
     products: [
       {
@@ -8,40 +11,43 @@ const points = [
         nameProduct: 'Shampoo 350ml',
         price: 6.40,
         CTA: 'cabelos Brilhantes',
-        location: 'Taguatinga',
+        location: 'Taguatinga'
       },
       {
         id: 2,
         nameProduct: 'Condicionador 3',
         price: 7.40,
         CTA: 'cabelos Brilhantes',
-        location: 'Taguatinga',
+        location: 'Taguatinga'
       },
       {
         id: 3,
         nameProduct: 'Sabonete 90g',
         price: 1.99,
         CTA: 'cabelos Brilhantes',
-        location: 'Taguatinga',
+        location: 'Taguatinga'
       },
       {
         id: 4,
         nameProduct: 'Creme Dental',
         price: 2.59,
         CTA: 'cabelos Brilhantes',
-        location: 'Taguatinga',
+        location: 'Taguatinga'
       },
       {
         id: 5,
         nameProduct: 'Escova de dente',
         price: 3.75,
         CTA: 'cabelos Brilhantes',
-        location: 'Taguatinga',
-      },
+        location: 'Taguatinga'
+      }
     ]
   },
   {
-    location: [-15.811785594695108, -48.01076462788768],
+    location: {
+      lat: -15.811785594695108,
+      lng: -48.01076462788768
+    },
     name: 'Atacadão dia a dia - EPTG',
     products: [
       {
@@ -49,38 +55,264 @@ const points = [
         nameProduct: 'Shampoo 350ml',
         price: 6.40,
         CTA: 'cabelos Brilhantes',
-        location: 'EPTG',
+        location: 'EPTG'
       },
       {
         id: 2,
         nameProduct: 'Condicionador 3',
         price: 7.40,
         CTA: 'cabelos Brilhantes',
-        location: 'EPTG',
+        location: 'EPTG'
       },
       {
         id: 3,
         nameProduct: 'Sabonete 90g',
         price: 1.99,
         CTA: 'cabelos Brilhantes',
-        location: 'EPTG',
+        location: 'EPTG'
       },
       {
         id: 4,
         nameProduct: 'Creme Dental',
         price: 2.59,
         CTA: 'cabelos Brilhantes',
-        location: 'EPTG',
+        location: 'EPTG'
       },
       {
         id: 5,
         nameProduct: 'Escova de dente',
         price: 3.75,
         CTA: 'cabelos Brilhantes',
-        location: 'EPTG',
-      },
+        location: 'EPTG'
+      }
+    ]
+  },
+  {
+    location: {
+      lat: -15.833510236582017,
+      lng: -48.00934570411195
+    },
+    name: 'Atacadão dia a dia - Águas Claras',
+    products: [
+      { id: 1, nameProduct: 'Sabão em pó 1kg', price: 12.90, CTA: 'Roupas mais brancas', location: 'Águas Claras' },
+      { id: 2, nameProduct: 'Amaciante 2L', price: 8.99, CTA: 'Perfume duradouro', location: 'Águas Claras' },
+      { id: 3, nameProduct: 'Desinfetante 1L', price: 5.50, CTA: 'Limpeza profunda', location: 'Águas Claras' },
+      { id: 4, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Águas Claras' },
+      { id: 5, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Águas Claras' }
+    ]
+  },
+  {
+    location: {
+      lat: -15.783792668936593,
+      lng: -48.12158200411286
+    },
+    name: 'Atacadão dia a dia - BR 070 (Ceilândia)',
+    products: [
+      { id: 1, nameProduct: 'Arroz 5kg', price: 19.90, CTA: 'Grãos selecionados', location: 'Águas Claras' },
+      { id: 2, nameProduct: 'Feijão 1kg', price: 7.99, CTA: 'Fonte de proteínas', location: 'Águas Claras' },
+      { id: 3, nameProduct: 'Desinfetante 1L', price: 5.50, CTA: 'Limpeza profunda', location: 'Águas Claras' },
+      { id: 4, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Águas Claras' },
+      { id: 5, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Águas Claras' }
+    ]
+  },
+  {
+    location: {
+      lat: -15.820342303401087,
+      lng: -48.102166219454126
+    },
+    name: 'Atacadão dia a dia - Ceilândia Centro',
+    products: [
+      { id: 1, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Ceilândia Centro' },
+      { id: 2, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Ceilândia Centro' },
+      { id: 3, nameProduct: 'Margarina 500g', price: 5.50, CTA: 'Cremosidade e sabor', location: 'Ceilândia Centro' },
+      { id: 4, nameProduct: 'Queijo mussarela 200g', price: 12.90, CTA: 'Sabor irresistível', location: 'Ceilândia Centro' },
+      { id: 5, nameProduct: 'Presunto 200g', price: 9.99, CTA: 'Pronto para o lanche', location: 'Ceilândia Centro' },
+      { id: 6, nameProduct: 'Iogurte 170g', price: 2.80, CTA: 'Fonte de probióticos', location: 'Ceilândia Centro' }
+    ]
+  },
+  {
+    location: {
+      lat: -15.833609171399777,
+      lng: -48.11312311945379
+    },
+    name: 'Atacadão dia a dia - Ceilândia Sul',
+    products: [
+      { id: 1, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - Ceilândia Sul' },
+      { id: 2, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - Ceilândia Sul' },
+      { id: 3, nameProduct: 'Margarina 500g', price: 5.50, CTA: 'Cremosidade e sabor', location: 'Atacadão dia a dia - Ceilândia Sul' },
+      { id: 4, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - Ceilândia Sul' },
+      { id: 5, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - Ceilândia Sul' }
+    ]
+  },
+  {
+    location: {
+      lat: -16.01459391647078,
+      lng: -48.059688726800026
+    },
+    name: 'Atacadão dia a dia - Gama',
+    products: [
+      { id: 1, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - Gama' },
+      { id: 2, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - Gama' },
+      { id: 3, nameProduct: 'Desinfetante 1L', price: 5.50, CTA: 'Limpeza profunda', location: 'Atacadão dia a dia - Gama' },
+      { id: 4, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - Gama' },
+      { id: 5, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - Gama' }
+    ]
+  },
+  {
+    location: {
+      lat: -15.825232941096864,
+      lng: -47.979263677124955
+    },
+    name: 'Atacadão dia a dia - Guará 2',
+    products: [
+      { id: 1, nameProduct: 'Sabão em pó 1kg', price: 12.90, CTA: 'Roupas mais brancas', location: 'Atacadão dia a dia - Guará 2' },
+      { id: 2, nameProduct: 'Amaciante 2L', price: 8.99, CTA: 'Perfume duradouro', location: 'Atacadão dia a dia - Guará 2' },
+      { id: 3, nameProduct: 'Desinfetante 1L', price: 5.50, CTA: 'Limpeza profunda', location: 'Atacadão dia a dia - Guará 2' },
+      { id: 4, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - Guará 2' },
+      { id: 5, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - Guará 2' }
+    ]
+  },
+  {
+    location: {
+      lat: -15.855147236882562,
+      lng: -47.817863304111306
+    },
+    name: 'Atacadão dia a dia - Jardim Botânico',
+    products: [
+      { id: 1, nameProduct: 'Arroz 5kg', price: 19.90, CTA: 'Grãos selecionados', location: 'Atacadão dia a dia - Jardim Botânico' },
+      { id: 2, nameProduct: 'Feijão 1kg', price: 7.99, CTA: 'Fonte de proteínas', location: 'Atacadão dia a dia - Jardim Botânico' },
+      { id: 3, nameProduct: 'Desinfetante 1L', price: 5.50, CTA: 'Limpeza profunda', location: 'Atacadão dia a dia - Jardim Botânico' },
+      { id: 4, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - Jardim Botânico' },
+      { id: 5, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - Jardim Botânico' }
+    ]
+  },
+  {
+    location: {
+      lat: -15.611411822519534,
+      lng: -47.69756926178719
+    },
+    name: 'Atacadão dia a dia - Mestre D’armas',
+    products: [
+      { id: 1, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - Mestre D’armas' },
+      { id: 2, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - Mestre D’armas' },
+      { id: 3, nameProduct: 'Margarina 500g', price: 5.50, CTA: 'Cremosidade e sabor', location: 'Atacadão dia a dia - Mestre D’armas' },
+      { id: 4, nameProduct: 'Queijo mussarela 200g', price: 12.90, CTA: 'Sabor irresistível', location: 'Atacadão dia a dia - Mestre D’armas' },
+      { id: 5, nameProduct: 'Presunto 200g', price: 9.99, CTA: 'Pronto para o lanche', location: 'Atacadão dia a dia - Mestre D’armas' },
+      { id: 6, nameProduct: 'Iogurte 170g', price: 2.80, CTA: 'Fonte de probióticos', location: 'Atacadão dia a dia - Mestre D’armas' }
+    ]
+  },
+  {
+    location: {
+      lat: -15.605121469305377,
+      lng: -47.654183953600075
+    },
+    name: 'Atacadão dia a dia - Planaltina',
+    products: [
+      { id: 1, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - Planaltina' },
+      { id: 2, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - Planaltina' },
+      { id: 3, nameProduct: 'Margarina 500g', price: 5.50, CTA: 'Cremosidade e sabor', location: 'Atacadão dia a dia - Planaltina' },
+      { id: 4, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - Planaltina' },
+      { id: 5, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - Planaltina' }
+    ]
+  },
+  {
+    location: {
+      lat: -15.90425524889405,
+      lng: -48.0734726617814
+    },
+    name: 'Atacadão dia a dia - Recanto das Emas',
+    products: [
+      { id: 1, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - Recanto das Emas' },
+      { id: 2, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - Recanto das Emas' },
+      { id: 3, nameProduct: 'Desinfetante 1L', price: 5.50, CTA: 'Limpeza profunda', location: 'Atacadão dia a dia - Recanto das Emas' },
+      { id: 4, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - Recanto das Emas' },
+      { id: 5, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - Recanto das Emas' }
+    ]
+  },
+  {
+    location: {
+      lat: -15.887777185631501,
+      lng: -48.08443208816785
+    },
+    name: 'Atacadão dia a dia - Samambaia I',
+    products: [
+      { id: 1, nameProduct: 'Sabão em pó 1kg', price: 12.90, CTA: 'Roupas mais brancas', location: 'Atacadão dia a dia - Samambaia' },
+      { id: 2, nameProduct: 'Amaciante 2L', price: 8.99, CTA: 'Perfume duradouro', location: 'Atacadão dia a dia - Samambaia' },
+      { id: 3, nameProduct: 'Desinfetante 1L', price: 5.50, CTA: 'Limpeza profunda', location: 'Atacadão dia a dia - Samambaia' },
+      { id: 4, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - Samambaia' },
+      { id: 5, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - Samambaia' }
+    ]
+  },
+  {
+    location: {
+      lat: -15.866697864365529,
+      lng: -48.056108290617686
+    },
+    name: 'Atacadão dia a dia - Samambaia II',
+    products: [
+      { id: 1, nameProduct: 'Sabão em pó 1kg', price: 12.90, CTA: 'Roupas mais brancas', location: 'Atacadão dia a dia - Samambaia II' },
+      { id: 2, nameProduct: 'Amaciante 2L', price: 8.99, CTA: 'Perfume duradouro', location: 'Atacadão dia a dia - Samambaia II' },
+      { id: 3, nameProduct: 'Desinfetante 1L', price: 5.50, CTA: 'Limpeza profunda', location: 'Atacadão dia a dia - Samambaia II' },
+      { id: 4, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - Samambaia II' },
+      { id: 5, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - Samambaia II' }
+    ]
+  },
+  {
+    location: {
+      lat: -15.802013610886956,
+      lng: -47.94797288692216
+    },
+    name: 'Atacadão dia a dia - SIA',
+    products: [
+      { id: 1, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - SIA' },
+      { id: 2, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - SIA' },
+      { id: 3, nameProduct: 'Desinfetante 1L', price: 5.50, CTA: 'Limpeza profunda', location: 'Atacadão dia a dia - SIA' },
+      { id: 4, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - SIA' },
+      { id: 5, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - SIA' }
+    ]
+  },
+  {
+    location: {
+      lat: -15.655868469172669,
+      lng: -47.78110283479921
+    },
+    name: 'Atacadão dia a dia - Sobradinho',
+    products: [
+      { id: 1, nameProduct: 'Sabão em pó 1kg', price: 12.90, CTA: 'Roupas mais brancas', location: 'Atacadão dia a dia - Sobradinho' },
+      { id: 2, nameProduct: 'Amaciante 2L', price: 8.99, CTA: 'Perfume duradouro', location: 'Atacadão dia a dia - Sobradinho' },
+      { id: 3, nameProduct: 'Desinfetante 1L', price: 5.50, CTA: 'Limpeza profunda', location: 'Atacadão dia a dia - Sobradinho' },
+      { id: 4, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - Sobradinho' },
+      { id: 5, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - Sobradinho' }
+    ]
+  },
+  {
+    location: {
+      lat: -15.794136329953023,
+      lng: -48.05301969408393
+    },
+    name: 'Atacadão dia a dia - Vicente Pires (Rua 12)',
+    products: [
+      { id: 1, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - Vicente Pires (Rua 12)' },
+      { id: 2, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - Vicente Pires (Rua 12)' },
+      { id: 3, nameProduct: 'Desinfetante 1L', price: 5.50, CTA: 'Limpeza profunda', location: 'Atacadão dia a dia - Vicente Pires (Rua 12)' },
+      { id: 4, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - Vicente Pires (Rua 12)' },
+      { id: 5, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - Vicente Pires (Rua 12)' }
+    ]
+  },
+  {
+    location: {
+      lat: -15.813046503001164,
+      lng: -48.016900348795886
+    },
+    name: 'Atacadão dia a dia - Vicente Pires (Rua 04)',
+    products: [
+      { id: 1, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - SIA' },
+      { id: 2, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - SIA' },
+      { id: 3, nameProduct: 'Desinfetante 1L', price: 5.50, CTA: 'Limpeza profunda', location: 'Atacadão dia a dia - SIA' },
+      { id: 4, nameProduct: 'Papel higiênico 12 rolos', price: 14.90, CTA: 'Toque macio', location: 'Atacadão dia a dia - SIA' },
+      { id: 5, nameProduct: 'Detergente 500ml', price: 2.30, CTA: 'Gordura zero', location: 'Atacadão dia a dia - SIA' }
     ]
   }
-]
+];
 
 export default points;
--- script.js
+++ script.js
@@ -4,30 +4,54 @@ let selectedPoint = {};
 let animationInterval;
 
 function configMap() {
-  const map = L.map('map').setView([-15.81476747189544, -48.01330789905051], 12);
+  const map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: -15.789569443413834, lng:  -47.89637032850101 },
+      zoom: 9.5,
+  });
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
-  }).addTo(map);
+
+  // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //   attribution: '&copy; OpenStreetMap contributors'
+  // }).addTo(map);
     
 
 
   points.forEach((p) => {
-    const marker = L.marker(p.location).addTo(map);
-    
-    const circle = L.circle(p.location, {
-        // color: "blue",
-        fillColor: "blue",
-        fillOpacity: 0.2,
-        radius: 500 // Raio de 1km
-    }).addTo(map);
+    const marker = new google.maps.Marker({
+        position: p.location,
+        map: map,
+        title: p.name
+    });
 
-    marker.on('click', () => {
-      createItensTable(p.location)
+    // const marker = L.marker(p.location).addTo(map);
+    
+    // const circle = L.circle(p.location, {
+    //     // color: "blue",
+    //     fillColor: "blue",
+    //     fillOpacity: 0.2,
+    //     radius: 500 // Raio de 1km
+    // }).addTo(map);
+
+    const circle = new google.maps.Circle({
+      strokeColor: '#FF0000', // Cor da borda
+      strokeOpacity: 0.8, // Opacidade da borda
+      strokeWeight: 0, // Espessura da borda
+      fillColor: '#FF0000', // Cor de preenchimento
+      fillOpacity: 0.35, // Opacidade do preenchimento
+      map: map,
+      center: p.location, // Centro do círculo
+      radius: 1000, // Raio em metros (1 km)
     });
-    circle.on('click', () => {
-      createItensTable(p.location)
+
+    marker.addListener('click', () => {
+      createItensTable(Object.values(p.location));
     });
+    circle.addListener('click', () => {
+      createItensTable(Object.values(p.location));
+    });
+    // circle.on('click', () => {
+    //   createItensTable(p.location)
+    // });
   });
 }
 
@@ -91,34 +115,48 @@ function animationProducts() {
 function createItensTable(location) {
   const tableContainer = document.getElementById('table');
   tableContainer.innerHTML = '';
-  selectedPoint = points.find((point) => `${point.location}` === `${location}`);
+  selectedPoint = points.find((point) => `${Object.values(point.location)}` === `${location}`);
 
   selectedPoint.products.forEach((product, index) => {
     createProductTD(product, index);
   });
-  if (selectedPoint) document.getElementById('add-product').classList.replace('hidden', 'block');
+  // if (selectedPoint) document.getElementById('add-product').classList.replace('hidden', 'block');
 
   animationProducts();
 }
 
-function addProduct() {
-  console.log(' as')
-}
+// function addProduct() {
+//   console.log(' as')
+// }
 
 function start() {
-  const addProcutButton = document.getElementById('add-product');
-  addProcutButton.addEventListener('click', addProduct());
+  // const addProcutButton = document.getElementById('add-product');
+  // addProcutButton.addEventListener('click', addProduct());
   
-  let selectedPreview = null;
-  document.querySelectorAll('.button-change-preview').forEach((button) => {
-    button.addEventListener('click', () => {
-      if (selectedPreview) {
-        selectedPreview.classList.remove('border-b-2', 'border-[#0D6AF4]', 'mb-[-2px]');
-      }
-      selectedPreview = button;
+  let selectedPreview = document.getElementById('Mobile'); // Inicia com o Mobile ativo
+
+  const previews = {
+    Mobile: document.getElementById('Mobile-previews'),
+    Desktop: document.getElementById('Desktop-previews')
+  };
+  
+  document.querySelectorAll('.button-change-preview').forEach((button, index) => {
+    if (index === 0) {
       button.classList.add('border-b-2', 'border-[#0D6AF4]', 'mb-[-2px]');
+      selectedPreview = button;
+    }
+    button.addEventListener('click', () => {
+      if (selectedPreview !== button) {
+        selectedPreview.classList.remove('border-b-2', 'border-[#0D6AF4]', 'mb-[-2px]');
+        previews[selectedPreview.id].classList.add('hidden');
+  
+        selectedPreview = button;
+        button.classList.add('border-b-2', 'border-[#0D6AF4]', 'mb-[-2px]');
+        previews[button.id].classList.remove('hidden');
+      }
     });
   });
+  
 
 
 
